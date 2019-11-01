d4dbed9fecb07153698f732f58d4fa88ccc34c3e
michaelmurwayi <https://github.com/michaelmurwayi>
2019-11-01 21:17:15 +0300

    challenge 1

--- chall-1.py
+++ chall-1.py
@@ -1,4 +1,4 @@
-def conv_hex_base64():
+def conv_hex_base64(string):
     # converts hex string to base64
     
     base64 = string.decode("hex").encode("base64")
@@ -7,4 +7,4 @@ def conv_hex_base64():
 
 if __name__ == "__main__":
     string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    conv_hex_base64()
+    conv_hex_base64(string)
