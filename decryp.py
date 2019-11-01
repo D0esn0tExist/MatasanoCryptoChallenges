42839c5a829ee0e6e0b382777c55d6f286ca61dc
michaelmurwayi <https://github.com/michaelmurwayi>
2019-11-01 20:41:48 +0300

    decrypt hex to base64

--- decryp.py
+++ decryp.py
@@ -1,10 +1,10 @@
 def conv_hex_base64():
     # converts hex string to base64
-
-    string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    
     base64 = string.decode("hex").encode("base64")
     print(base64)
     return base64
 
 if __name__ == "__main__":
+    string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     conv_hex_base64()
