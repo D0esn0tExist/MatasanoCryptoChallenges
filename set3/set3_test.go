98908ec355471592f3db57adcf2da4fffbae8db9
Levanto <evatom86@gmail.com>
2020-10-30 22:34:20 +0300

    CTR: Implement AES Counter mode (#23)
    * CTR: Implement AES Counter mode
    
    * CTR: Add decrypted message test

--- set3/set3_test.go
+++ set3/set3_test.go
@@ -1,6 +1,7 @@
 package set3
 
 import (
+	"encoding/base64"
 	"log"
 	"math/rand"
 	"testing"
@@ -34,5 +35,34 @@ func TestPadOracleAttack(t *testing.T) {
 	log.Printf("Decrypted bytes: %v", decryptedMsg)
 	if decryptedMsg != message {
 		t.Errorf("AttackOracle() fail. Expected: %v. Found: %v", message, decryptedMsg)
+	}
+}
+
+func TestCtrMode(t *testing.T) {
+	testCipher := "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
+	testCipherBytes, err := base64.StdEncoding.DecodeString(testCipher)
+	if err != nil {
+		panic("Error decoding string")
+	}
+	keyBytes := []byte("YELLOW SUBMARINE")
+
+	// rule on how to update nonce on each block. For this test, update the 9th byte incrementally from 0.
+	nonceRule := func(blockNumber int, nonce []byte) []byte {
+		nonce[8] = byte(blockNumber)
+		return nonce
+	}
+
+	expectedMessage := "Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby "
+	decryptedBytes := CtrMode(keyBytes, testCipherBytes, nonceRule)
+	if string(decryptedBytes) != expectedMessage {
+		t.Errorf("Decrytion fail. Expected: %v. Got: %v", expectedMessage, string(decryptedBytes))
+	}
+
+	encryptedBytes := CtrMode(keyBytes, decryptedBytes, nonceRule)
+	testCipherResult := base64.StdEncoding.EncodeToString(encryptedBytes)
+
+	logger.Printf("Encrypted message: %v", testCipherResult)
+	if testCipherResult != testCipher {
+		t.Errorf("CtrMode() fail. Expected: %v. Found: %v", testCipher, testCipherResult)
 	}
 }
